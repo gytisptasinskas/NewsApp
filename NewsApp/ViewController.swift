ac6005dc05b7675eaefbf476505c1c2aa247af41
Gytis Ptašinskas <51530980+gytisptasinskas@users.noreply.github.com>
2023-11-21 15:00:11 +0200

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Gytis Ptašinskas on 21/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
